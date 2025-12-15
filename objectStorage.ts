e33351b601a135d23b3dd299b0a541acd5390c13
Dani Sofyan <danixsofyan@gmail.com>
2025-12-15 14:18:08 +0700

    Update objectStorage.ts

--- objectStorage.ts
+++ objectStorage.ts
@@ -4,7 +4,7 @@ dotenv.config();
 
 const objectStorageAccessKeyId = process.env.OBJECT_STORAGE_ACCESS_KEY_ID || "";
 const objectStorageAccessKeySecret = process.env.OBJECT_STORAGE_ACCESS_KEY_SECRET || "";
-const objectStorageEndpoint = process.env.OBJECT_STORAGE_END_POINT || "";
+const objectStorageEndpoint = process.env.OBJECT_STORAGE_PUBLIC_END_POINT || "";
 const objectStorageRegion = process.env.S3_REGION || "sin1"
 
 const bucketS3 = new S3Client({
