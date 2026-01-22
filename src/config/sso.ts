881498e2e5e27e277d7302da29df58acf5f6859c
Restu Dwi Cahyo <restu@resitdc.id>
2026-01-22 22:50:19 +0800

    Add sso config

---
+++ src/config/sso.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+import dotenv from "dotenv";
+import errorResponseHandler from "@middlewares/axiosMiddleware";
+
+dotenv.config();
+const ssoHost: string | undefined = process.env.SSO_BASE_URL;
+
+const ssoInstance = (token?: string) => {
+  const instance = axios.create({
+    baseURL: ssoHost,
+  });
+
+  instance.interceptors.request.use(
+    (config) => {
+      if (token) {
+        const bearerToken = `Bearer ${token}`;
+        config.headers.Authorization = bearerToken;
+      }
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+
+  instance.interceptors.response.use((response) => response, errorResponseHandler);
+
+  return instance;
+};
+
+export default ssoInstance;
