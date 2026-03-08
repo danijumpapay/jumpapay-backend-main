9e05b8210ba9fee2ae29cc972b9f503728ac8090
Dani Sofyan <danixsofyan@gmail.com>
2026-03-09 02:19:19 +0700

    refactor: simplify tsconfig type declarations, enable direct TypeScript execution for dev, and improve Express declaration merging.

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,11 +1,8 @@
-import { Request } from "express";
-import { AccessTokenPayload } from "@dataTypes/general"; 
+import { AccessTokenPayload } from "@dataTypes/general";
 
-declare global {
-  namespace Express {
-    interface Request {
-      files?: Express.Multer.File[] | { [fieldname: string]: Express.Multer.File[] };
-      currentUser?: AccessTokenPayload;
-    }
+declare module 'express' {
+  export interface Request {
+    files?: Express.Multer.File[] | { [fieldname: string]: Express.Multer.File[] };
+    currentUser?: AccessTokenPayload;
   }
 }
